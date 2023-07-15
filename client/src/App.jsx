8fed523f000ee5bd8ccd966d1b3e5ca161b152ed
Saunak <76943154+Saunakghosh10@users.noreply.github.com>
2023-07-16 01:00:31 +0530

    Correct import statements and error handling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,31 @@
-import React from 'react'
-import {BrowserRouter,Link,Route,Routes} from 'react-router-dom';
-import {AI} from './assets';
-import {Home,CreatePost} from './pages';
+import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { AI } from "./assets";
+import { Home, CreatePost } from "./pages";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <header className='w-full flex justify-between items-center bg-blue-300 sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
-    <Link to='/'>
-      <img src={AI} alt="logo" className='w-24 ml-2 object-contain' />
-    </Link>
-    <Link to='/create-post' className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'> Create</Link>
-    </header>
-    <main className='sm:p-8 px-4 py-8 w-full bg-[#d6e8ea] min-h-[calc(100vh-73px)]'>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/create-post' element={<CreatePost/>} />
-      </Routes>
-    </main>
+      <header className="w-full flex justify-between items-center bg-blue-300 sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+        <Link to="/">
+          <img src={AI} alt="logo" className="w-24 ml-2 object-contain" />
+        </Link>
+        <Link
+          to="/create-post"
+          className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md"
+        >
+          {" "}
+          Create
+        </Link>
+      </header>
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#d6e8ea] min-h-[calc(100vh-73px)]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-post" element={<CreatePost />} />
+        </Routes>
+      </main>
     </BrowserRouter>
-  )
-}
- 
-export default App
+  );
+};
 
-
-
-
-
-
-
+export default App;
